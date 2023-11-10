8d4644bfbfbd666e0f12e90ff2664565330c7657
Zonetto <as076053@gmail.com>
2023-11-10 10:54:48 +0300

    UPD create_account_form.dart & sign_in_form.dart

--- lib/components/form/create_account_form.dart
+++ lib/components/form/create_account_form.dart
@@ -1,6 +1,4 @@
-import 'package:announcement_of_services/components/custom_text.dart';
 import 'package:announcement_of_services/components/custom_text_form_field.dart';
-import 'package:announcement_of_services/utils/constant/font_size.dart';
 import 'package:announcement_of_services/utils/date_picker.dart';
 import 'package:announcement_of_services/utils/navigate_utils.dart';
 import 'package:announcement_of_services/view/add_photo_screen.dart';
@@ -14,90 +12,89 @@ class CreateAccountForm extends StatefulWidget {
   State<CreateAccountForm> createState() => _CreateAccountFormState();
 }
 
-TextEditingController controller = TextEditingController();
+final GlobalKey<FormState> _personFormKey = GlobalKey<FormState>();
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _callController = TextEditingController();
+final TextEditingController _dateOfBirthController = TextEditingController();
+final TextEditingController _passwordController = TextEditingController();
+final TextEditingController _confirmPasswordController =
+    TextEditingController();
 
 class _CreateAccountFormState extends State<CreateAccountForm> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 10.0,
-        ),
-        const CustomText(
-          title: 'سجل الأن',
-          fontWeight: FontWeight.bold,
-          size: FontSize.headline,
-        ),
-        // const Spacer(),
-        Expanded(
-          child: ListView(
-            children: [
-              CustomTextFormFiled(
-                context: context,
-                controller: controller,
-                headline: 'عنوان البريد الألكتروني',
-                hintText: 'إدخل أسم المستخدم',
-                textInputType: TextInputType.name,
-                valid: (val) {},
-                icon: const Icon(Icons.person),
-              ),
-              CustomTextFormFiled(
-                context: context,
-                controller: controller,
-                headline: 'رقم الهاتف',
-                hintText: 'إدخل رقم الهاتف',
-                textInputType: TextInputType.phone,
-                valid: (val) {},
-                icon: const Icon(Icons.phone),
-              ),
-              CustomTextFormFiled(
-                context: context,
-                controller: controller,
-                headline: 'تاريخ الميلاد',
-                hintText: 'إدخل تاريخ ميلادك',
-                textInputType: TextInputType.number,
-                readOnly: true,
-                valid: (val) {},
-                onTab: () {
-                  datePicker(context: context);
-                },
-                icon: const Icon(Icons.date_range_outlined),
-              ),
-              CustomTextFormFiled(
-                context: context,
-                controller: controller,
-                headline: 'كلمة المرور',
-                hintText: 'إدخل كلمة المرور',
-                textInputType: TextInputType.visiblePassword,
-                valid: (val) {},
-                icon: const Icon(Icons.password_outlined),
-              ),
-              CustomTextFormFiled(
-                context: context,
-                controller: controller,
-                headline: 'تأكيد كلمة المرور',
-                hintText: 'إدخل تأكيد كلمة المرور',
-                textInputType: TextInputType.visiblePassword,
-                valid: (val) {},
-                icon: const Icon(Icons.password_outlined),
-              ),
-            ],
+    return Form(
+      key: _personFormKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _emailController,
+                  headline: 'عنوان البريد الألكتروني',
+                  hintText: 'إدخل البريد الألكتروني',
+                  textInputType: TextInputType.name,
+                  valid: (val) {},
+                  icon: const Icon(Icons.person),
+                ),
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _callController,
+                  headline: 'رقم الهاتف',
+                  hintText: 'إدخل رقم الهاتف',
+                  textInputType: TextInputType.phone,
+                  valid: (val) {},
+                  icon: const Icon(Icons.phone),
+                ),
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _dateOfBirthController,
+                  headline: 'تاريخ الميلاد',
+                  hintText: 'إدخل تاريخ ميلادك',
+                  textInputType: TextInputType.number,
+                  readOnly: true,
+                  valid: (val) {},
+                  onTab: () async {
+                    await datePicker(context: context);
+                  },
+                  icon: const Icon(Icons.date_range_outlined),
+                ),
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _passwordController,
+                  headline: 'كلمة المرور',
+                  hintText: 'إدخل كلمة المرور',
+                  textInputType: TextInputType.visiblePassword,
+                  valid: (val) {},
+                  icon: const Icon(Icons.password_outlined),
+                ),
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _confirmPasswordController,
+                  headline: 'تأكيد كلمة المرور',
+                  hintText: 'إدخل تأكيد كلمة المرور',
+                  textInputType: TextInputType.visiblePassword,
+                  valid: (val) {},
+                  icon: const Icon(Icons.password_outlined),
+                ),
+              ],
+            ),
           ),
-        ),
-        // const Spacer(),
-        CustomButton(
-          context: context,
-          title: 'إنشاء حساب',
-          onPressed: () {
-            navigatePushScreen(
-              context: context,
-              screen: const AddPhotoScreen(),
-            );
-          },
-        ),
-      ],
+          CustomButton(
+            context: context,
+            title: 'إنشاء حساب',
+            onPressed: () {
+              navigatePushScreen(
+                context: context,
+                screen: const AddPhotoScreen(),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/components/form/sign_in_form.dart
+++ lib/components/form/sign_in_form.dart
@@ -1,6 +1,4 @@
-import 'package:announcement_of_services/components/custom_text.dart';
 import 'package:announcement_of_services/components/custom_text_form_field.dart';
-import 'package:announcement_of_services/utils/constant/font_size.dart';
 import 'package:announcement_of_services/components/custom_buttom.dart';
 import 'package:flutter/material.dart';
 
@@ -11,55 +9,49 @@ class SignInForm extends StatefulWidget {
   State<SignInForm> createState() => _SignInFormState();
 }
 
-TextEditingController controller = TextEditingController();
+final GlobalKey<FormState> _personFormKey = GlobalKey<FormState>();
+final TextEditingController _emailController = TextEditingController();
+final TextEditingController _passwordController = TextEditingController();
 
 class _SignInFormState extends State<SignInForm> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 10.0,
-        ),
-        const CustomText(
-          title: 'مرحباً بعودتك',
-          fontWeight: FontWeight.bold,
-          size: FontSize.headline,
-        ),
-        const Spacer(),
-        Column(
-          children: [
-            CustomTextFormFiled(
-              context: context,
-              controller: controller,
-              headline: 'عنوان البريد الألكتروني',
-              hintText: 'إدخل أسم المستخدم',
-              textInputType: TextInputType.name,
-              valid: (val) {},
-              icon: const Icon(Icons.person),
+    return Form(
+      key: _personFormKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _emailController,
+                  headline: 'عنوان البريد الألكتروني',
+                  hintText: 'إدخل البريد الألكتروني',
+                  textInputType: TextInputType.name,
+                  valid: (val) {},
+                  icon: const Icon(Icons.person),
+                ),
+                CustomTextFormFiled(
+                  context: context,
+                  controller: _passwordController,
+                  headline: 'كلمة المرور',
+                  hintText: 'إدخل كلمة المرور',
+                  textInputType: TextInputType.visiblePassword,
+                  valid: (val) {},
+                  icon: const Icon(Icons.password_outlined),
+                ),
+              ],
             ),
-            // const SizedBox(
-            //   height: 10.0,
-            // ),
-            CustomTextFormFiled(
-              context: context,
-              controller: controller,
-              headline: 'كلمة المرور',
-              hintText: 'إدخل كلمة المرور',
-              textInputType: TextInputType.visiblePassword,
-              valid: (val) {},
-              icon: const Icon(Icons.password_outlined),
-            ),
-          ],
-        ),
-        const Spacer(),
-        CustomButton(
-          context: context,
-          title: 'تسجيل الدخول',
-          onPressed: () {},
-        ),
-      ],
+          ),
+          CustomButton(
+            context: context,
+            title: 'تسجيل الدخول',
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
