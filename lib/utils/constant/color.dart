7d4fc4b2b4f0cf988ee917db96fccafac546c134
Zonetto <as076053@gmail.com>
2023-11-18 19:43:49 +0300

    UPD color.dart

--- lib/utils/constant/color.dart
+++ lib/utils/constant/color.dart
@@ -7,4 +7,10 @@ class AppColor {
   static const Color colorTextButtonWhite = Colors.white;
   static const Color colorTextButtonGreen = Color(0xFF539165);
   static const Color shadowColor = Color(0x3F000000);
+  static const BoxShadow shadow = BoxShadow(
+    color: AppColor.shadowColor,
+    blurRadius: 4,
+    offset: Offset(0, 4),
+    spreadRadius: 0,
+  );
 }
