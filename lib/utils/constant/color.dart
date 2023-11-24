34340f68b1428ccdf6fa5ad2b5c06b3670f58ddc
Zonetto <as076053@gmail.com>
2023-11-24 08:51:09 +0300

    UPD size.dart & color.dart

--- lib/utils/constant/color.dart
+++ lib/utils/constant/color.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
+  static const Color colorTextBlack = Colors.black;
   static const Color screenBackGroundColor = Color(0xFFF0F5F2);
   static const Color buttonColorGreen = Color(0xFF539165);
   static const Color buttonColorGrey = Color(0xFFD9D9D9);
--- lib/utils/constant/size.dart
+++ lib/utils/constant/size.dart
@@ -1,4 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'responsive_screen.dart';
+
 class AppSize {
-  static const double horizontalMargin = 10.0;
-  static const double verticalMargin = 20.0;
+  static const EdgeInsets nternalMargin = EdgeInsets.symmetric(
+    horizontal: 20.0,
+    vertical: 20.0,
+  );
+  static const EdgeInsets padding = EdgeInsets.symmetric(
+    horizontal: 10.0,
+    vertical: 20.0,
+  );
+
+  static double imageSizeSmall(BuildContext context) =>
+      Dimensions.screenWidth(context) / 12.0;
+  static double imageSizeLarg(BuildContext context) =>
+      Dimensions.screenWidth(context) / 3.0;
+
+//  static const double internalMargin = 20.0;
 }
