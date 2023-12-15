55f113eb2e76e63fa95f2c7a4b744f9124cd19b8
Zonetto <as076053@gmail.com>
2023-12-15 22:33:13 +0300

    UPD main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,74 @@
+import 'dart:io';
+
+import 'package:announcement_of_services/view/complex_screen.dart';
 import 'package:announcement_of_services/view/start_screen.dart';
+import 'package:announcement_of_services/view_model/view_model_auth.dart';
+import 'package:announcement_of_services/view_model/view_model_fetch.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 import 'generated/l10n.dart';
-import 'view/add_photo_screen.dart';
+import 'view/categories_creen.dart';
+import 'view/home_screen.dart';
+import 'view/loadingScreen.dart';
+import 'view/registration_option_screen.dart';
+import 'view/service_details_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey: 'AIzaSyCnSlXad2dN_lYXEjg15uuY8C18L2hchqw',
+          appId: '1:173236485210:android:95e8fab76659b783faac63',
+          messagingSenderId: '173236485210',
+          projectId: 'flutterhomeservice-73056',
+        ))
+      : await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider('AIzaSyCnSlXad2dN_lYXEjg15uuY8C18L2hchqw'),
+    appleProvider: AppleProvider.appAttest,
+    androidProvider: AndroidProvider.debug,
+  );
+
   runApp(
-    MaterialApp(
-      locale: const Locale('ar'),
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
+    // Builder(
+    //   builder: (context) => MultiProvider(
+    //     providers: [
+    //       ChangeNotifierProvider<ViewModelAuth>(
+    //         create: (context) => ViewModelAuth(),
+    //         // child: MaterialApp(
+    //         //   locale: const Locale('ar'),
+    //         //   localizationsDelegates: const [
+    //         //     S.delegate,
+    //         //     GlobalMaterialLocalizations.delegate,
+    //         //     GlobalWidgetsLocalizations.delegate,
+    //         //     GlobalCupertinoLocalizations.delegate
+    //         //   ],
+    //         //   debugShowCheckedModeBanner: false,
+    //         //   supportedLocales: S.delegate.supportedLocales,
+    //         //   home: const MainApp(), // Use MainApp as the home widget
+    //         // ),
+    //       ),
+    //       ChangeNotifierProvider<ViewModelFetch>(
+    //         create: (context) => ViewModelFetch(),
+    //       )
+    //     ],
+    //   ),
+    // ),
+
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ViewModelAuth>(
+          create: (_) => ViewModelAuth(),
+        ),
+        ChangeNotifierProvider<ViewModelFetch>(
+          create: (_) => ViewModelFetch(),
+        )
       ],
-      debugShowCheckedModeBanner: false,
-      supportedLocales: S.delegate.supportedLocales,
-      home: const MainApp(), // Use MainApp as the home widget
+      child: const MainApp(),
     ),
   );
 }
@@ -26,6 +78,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const StartScreen();
+    //   return MaterialApp(home: const StartScreen());
+
+    return Consumer<ViewModelAuth>(
+      builder: (ctx, auth, _) => MaterialApp(
+        locale: const Locale('ar'),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        debugShowCheckedModeBanner: false,
+        supportedLocales: S.delegate.supportedLocales,
+        home: auth.isAuth!
+            ? const ComplexScreen()
+            : FutureBuilder(
+                future: auth.tryAuthLogin(),
+                builder: (ctx, snapShot) =>
+                    (snapShot.connectionState == ConnectionState.waiting)
+                        ? const LoadingScreen()
+                        : const StartScreen(),
+              ), // Use MainApp as the home widget
+      ),
+    );
   }
 }
