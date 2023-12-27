00e7e52feb4231ce29a6f13ac645a0fcf5df8d07
Zonetto <as076053@gmail.com>
2023-12-27 10:25:22 +0300

    UPD lib/view_model/view_model_auth.dart

--- lib/view_model/view_model_auth.dart
+++ lib/view_model/view_model_auth.dart
@@ -4,11 +4,27 @@ import 'dart:io';
 import 'package:announcement_of_services/components/shared/network/local/cache_helper.dart';
 import 'package:announcement_of_services/module/auth_model.dart';
 import 'package:announcement_of_services/module/user_model.dart';
-import 'package:announcement_of_services/services/fire_database_servises.dart';
+import 'package:announcement_of_services/services/collections/user_collection.dart';
 import 'package:announcement_of_services/services/fire_storage_servises.dart';
+import 'package:announcement_of_services/utils/constant/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+
+import 'app_status.dart';
+
+class UserManager {
+  static final UserManager _instance = UserManager._internal();
+  late String userId;
+
+  factory UserManager() {
+    return _instance;
+  }
+
+  UserManager._internal();
+}
 
 abstract class Token {
   String? _token;
@@ -28,77 +44,80 @@ abstract class Token {
 class ViewModelAuth extends Token with ChangeNotifier {
   Timer? _authTimer;
   bool isLoad = true;
-
-  UserModel? _userDetails;
-
-  UserModel? get getUserDetails => _userDetails;
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   FireStorageServises fireStorageServises = FireStorageServises();
   bool? get isAuth => token != null;
-
-  Future<bool> _authenticate({
+  Response? _res;
+  Future<Result> _authenticate({
     required String email,
     required String password,
     UserModel? user,
     required String urlSegment,
     File? photo,
   }) async {
-    final url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAte23mrZFcs16b_FufGlGesH_TCP7zPBc";
+    final fullUrl = "$basUrl/accounts:$urlSegment?$key";
 
     try {
-      var res = await http.post(Uri.parse(url),
-          body: jsonEncode({
+      _res = await http.post(
+        Uri.parse(fullUrl),
+        body: jsonEncode(
+          {
             'email': email,
             'password': password,
             'returnSecureToken': true,
-          }));
-      final Map<String, dynamic> resData = json.decode(res.body);
+          },
+        ),
+      );
+
+      // if (urlSegment == "signInWithPassword") {
+      //   firebaseAuth.signInWithEmailAndPassword(
+      //     email: email,
+      //     password: password,
+      //   );
+      // }
+
+      final Map<String, dynamic> resData = json.decode(_res!.body);
+      final String uid = resData['localId'];
+
+      //firebaseAuth.currentUser!.uid.toString(); //resData['localId'];
+
       if (resData['error'] != null) {
         print("${resData['error']['message']}");
         throw "${resData['error']['message']}";
       }
-      _token = resData['email'];
-      _userId = resData['localId'];
+      // _token = resData['email'];
+      _userId = uid;
+      UserManager().userId = uid;
       _expiryDate =
           DateTime.now().add(Duration(hours: int.parse(resData['expiresIn'])));
       _autoLogout();
       if (urlSegment == "signUp") {
-        fireStorageServises.init();
-        await fireStorageServises.upload(photo!);
-        String? downloadUrl = await fireStorageServises.getImageUrl();
-        // String? downloadUrl =
-        //     await fireStorageServises.uploadImageToFirebaseStorage(photo!);
+        //  await fireStorageServises.upload(photo!);
+        // firebaseAuth.createUserWithEmailAndPassword(
+        //   email: email,
+        //   password: password,
+        // );
+        String? downloadUrl = await fireStorageServises
+            .uploadAndGetImageToFirebaseStorage(photo!);
         user!.image = downloadUrl;
-        user.userId = resData['localId'];
-        FireDatabaseServises().addUserInfoDB(_userId!, user.toJson());
+        user.userId = _userId;
+        await Future.delayed(const Duration(seconds: 3));
+        UserCollection().addInfoDB(doc: uid, info: user.toJson());
       }
 
       notifyListeners();
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   SnackBar(
-      //     duration: const Duration(seconds: 5),
-      //     backgroundColor: AppColor.colorTextButtonGreen,
-      //     content: Text(
-      //       "You are logged in with an account: $_token",
-      //       textAlign: TextAlign.center,
-      //       style: const TextStyle(
-      //         color: Colors.white,
-      //         fontSize: 14.0,
-      //       ),
-      //     ),
-      //   ),
-      // );
-
-      // CacheHelper.init();
-      await CacheHelper.putDataString(key: 'token', value: _token);
+      //   await CacheHelper.putDataString(key: 'token', value: _token);
       await CacheHelper.putDataString(key: 'userId', value: _userId);
       await CacheHelper.putDataString(
           key: 'expiryDate', value: _expiryDate!.toIso8601String());
+      return Success(code: _res!.statusCode, response: resData);
     } catch (e) {
-      return false;
+      return Error(code: _res!.statusCode, errorResponse: e.toString());
+      //return false;
       // rethrow;
     }
-    return true;
+
+    // return true;
   }
 
   Future<bool> tryAuthLogin() async {
@@ -119,41 +138,46 @@ class ViewModelAuth extends Token with ChangeNotifier {
     _expiryDate = expiryDate;
     _token = CacheHelper.getDataString(key: 'token') ?? "";
     _userId = CacheHelper.getDataString(key: 'userId') ?? "";
+    UserManager().userId = _userId ?? "";
     _autoLogout();
     notifyListeners();
 
     return true;
   }
 
-  Future<bool> signUp(
+  Future<Result> signUp(
     UserModel authUser,
     File? photo,
   ) async {
-    final bool auth = await _authenticate(
+    final auth = _authenticate(
       email: authUser.email,
       password: authUser.password,
       user: authUser,
       urlSegment: "signUp",
       photo: photo,
     );
-    print(auth);
-    if (!auth) {
-      return false;
-    }
-    return true;
+    // print(auth);
+    // if (!auth) {
+    //   return false;
+    // }
+    return auth;
   }
 
-  Future<bool> logIn(AuthModel authUser) async {
-    final bool auth = await _authenticate(
+  Future<Result> logIn(AuthModel authUser) async {
+    // try {
+    final auth = await _authenticate(
       email: authUser.email,
       password: authUser.password,
       urlSegment: "signInWithPassword",
     );
-    print(auth);
-    if (!auth) {
-      return false;
-    }
-    return true;
+    return auth;
+    //  } catch (e) {}
+
+    //print(auth);
+    // if (!auth) {
+    //   return false;
+    // }
+    // return true;
   }
 
   void logOut() async {
