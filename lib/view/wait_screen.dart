b4d2f5af0366c6c3bd1933ea544012c33b5d5fd2
Zonetto <as076053@gmail.com>
2023-12-27 10:13:30 +0300

    ADD wait_screen.dart

---
+++ lib/view/wait_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class WaitScreen extends StatelessWidget {
+  const WaitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('رجاءًا أنتظر لحين موافقة الادمن على خدماتك'),
+    );
+  }
+}
