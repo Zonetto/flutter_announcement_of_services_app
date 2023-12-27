3203dfb7612b6ef27e024f2cec3ca2965bb4d7ab
Zonetto <as076053@gmail.com>
2023-12-27 10:17:41 +0300

    UPD create_account_form.dart & sign_in_form.dart

--- lib/components/form/create_account_form.dart
+++ lib/components/form/create_account_form.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 import 'package:announcement_of_services/components/custom_text_form_field.dart';
 import 'package:announcement_of_services/module/user_model.dart';
+import 'package:announcement_of_services/utils/constant/color.dart';
+import 'package:announcement_of_services/utils/constant/constants.dart';
 import 'package:announcement_of_services/utils/date_picker.dart';
 import 'package:announcement_of_services/components/custom_buttom.dart';
 import 'package:announcement_of_services/utils/navigate_utils.dart';
 import 'package:announcement_of_services/utils/pick_image.dart';
+import 'package:announcement_of_services/utils/styled_scaffold_messenger.dart';
+import 'package:announcement_of_services/utils/validate.dart';
 import 'package:announcement_of_services/view/complex_screen.dart';
+import 'package:announcement_of_services/view_model/app_status.dart';
 import 'package:announcement_of_services/view_model/view_model_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -18,16 +23,41 @@ class CreateAccountForm extends StatefulWidget {
   State<CreateAccountForm> createState() => _CreateAccountFormState();
 }
 
-final GlobalKey<FormState> _logUpFormKey = GlobalKey<FormState>();
-final TextEditingController _emailController = TextEditingController();
-final TextEditingController _fullNameController = TextEditingController();
-final TextEditingController _callController = TextEditingController();
-final TextEditingController _dateOfBirthController = TextEditingController();
-final TextEditingController _passwordController = TextEditingController();
-final TextEditingController _confirmPasswordController =
-    TextEditingController();
-
 class _CreateAccountFormState extends State<CreateAccountForm> {
+  final GlobalKey<FormState> _logUpFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _callController = TextEditingController();
+  final TextEditingController _dateOfBirthController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  bool _isObscureText = true;
+  bool _loodaing = false;
+
+  @override
+  void dispose() {
+    _logUpFormKey.currentState?.dispose();
+    _emailController.dispose();
+    _fullNameController.dispose();
+    _callController.dispose();
+    _dateOfBirthController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  // @override
+  // void initState() {
+  //    _logUpFormKey.currentState?.dispose();
+  //   _emailController.dispose();
+  //   _fullNameController.dispose();
+  //   _callController.dispose();
+  //   _dateOfBirthController.dispose();
+  //   _passwordController.dispose();
+  //   _confirmPasswordController.dispose();
+  //   super.initState();
+  // }
   File? _selectedImage;
   @override
   Widget build(BuildContext context) {
@@ -75,8 +105,10 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
                                     final XFile? selectedImage =
                                         await pickImage(ImageSource.gallery);
                                     setState(() {
-                                      _selectedImage =
-                                          File(selectedImage!.path);
+                                      if (selectedImage != null) {
+                                        _selectedImage =
+                                            File(selectedImage.path);
+                                      }
                                     });
                                   },
                                   icon: const Icon(Icons.camera_alt_rounded),
@@ -94,8 +126,11 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
                     controller: _emailController,
                     headline: 'عنوان البريد الألكتروني',
                     hintText: 'إدخل البريد الألكتروني',
+                    readOnly: _loodaing ? true : false,
                     textInputType: TextInputType.emailAddress,
-                    valid: (val) {},
+                    valid: (String? value) {
+                      return ValidTextForm().email(value);
+                    },
                     icon: const Icon(Icons.person),
                   ),
                   CustomTextFormFiled(
@@ -103,8 +138,11 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
                     controller: _fullNameController,
                     headline: 'الاسم',
                     hintText: 'إدخل الاسم',
+                    readOnly: _loodaing ? true : false,
                     textInputType: TextInputType.name,
-                    valid: (val) {},
+                    valid: (String? value) {
+                      return ValidTextForm().emptyText(value);
+                    },
                     icon: const Icon(Icons.person),
                   ),
                   CustomTextFormFiled(
@@ -112,8 +150,11 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
                     controller: _callController,
                     headline: 'رقم الهاتف',
                     hintText: 'إدخل رقم الهاتف',
+                    readOnly: _loodaing ? true : false,
                     textInputType: TextInputType.phone,
-                    valid: (val) {},
+                    valid: (String? value) {
+                      return ValidTextForm().call(value);
+                    },
                     icon: const Icon(Icons.phone),
                   ),
                   CustomTextFormFiled(
@@ -123,61 +164,111 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
                     hintText: 'إدخل تاريخ ميلادك',
                     textInputType: TextInputType.number,
                     readOnly: true,
-                    valid: (val) {},
-                    onTab: () async {
-                      final String date = await datePicker(context: context);
-                      _dateOfBirthController.text = date;
+                    valid: (String? value) {
+                      return ValidTextForm().emptyText(value);
                     },
+                    onTab: !_loodaing
+                        ? () async {
+                            final String date =
+                                await datePicker(context: context);
+                            _dateOfBirthController.text = date;
+                          }
+                        : null,
                     icon: const Icon(Icons.date_range_outlined),
                   ),
                   CustomTextFormFiled(
                     context: context,
+                    isPassword: true,
+                    obscureText: _isObscureText,
+                    readOnly: _loodaing ? true : false,
+                    onPressedPassword: () {
+                      setState(() {
+                        _isObscureText = !_isObscureText;
+                      });
+                    },
                     controller: _passwordController,
                     headline: 'كلمة المرور',
                     hintText: 'إدخل كلمة المرور',
                     textInputType: TextInputType.visiblePassword,
-                    valid: (val) {},
+                    valid: (String? value) {
+                      return ValidTextForm().password(value);
+                    },
                     icon: const Icon(Icons.password_outlined),
                   ),
                   CustomTextFormFiled(
                     context: context,
+                    isPassword: true,
+                    obscureText: _isObscureText,
+                    readOnly: _loodaing ? true : false,
+                    onPressedPassword: () {
+                      setState(() {
+                        _isObscureText = !_isObscureText;
+                      });
+                    },
                     controller: _confirmPasswordController,
                     headline: 'تأكيد كلمة المرور',
                     hintText: 'إدخل تأكيد كلمة المرور',
                     textInputType: TextInputType.visiblePassword,
-                    valid: (val) {},
+                    valid: (String? value) {
+                      return ValidTextForm().confirmPassword(
+                        value,
+                        _passwordController.text,
+                      );
+                    },
                     icon: const Icon(Icons.password_outlined),
                   ),
                   //   Spacer(),
                   CustomButton(
+                    isLoad: _loodaing,
                     context: context,
                     title: 'إنشاء حساب',
                     onPressed: () async {
+                      if (!_logUpFormKey.currentState!.validate()) {
+                        return;
+                      }
+                      setState(() {
+                        _loodaing = true;
+                      });
+
+                      _logUpFormKey.currentState!.save();
                       final String email = _emailController.text.trim();
+                      //  'a44hmedee@gmail.com'; //_emailController.text.trim();
                       String fullName = _fullNameController.text.trim();
+                      //  'Ahmed'; //_fullNameController.text.trim();
                       final String password = _passwordController.text.trim();
+                      //  '112233'; //_passwordController.text.trim();
                       final String dateOfBirth =
-                          _dateOfBirthController.text.trim();
+                          _dateOfBirthController.text.trim(); //
+                      _dateOfBirthController.text.trim(); // '2002';
+                      // _dateOfBirthController.text.trim();
                       final int call = int.parse(_callController.text.trim());
-                      bool isServiceProvider = false;
+                      // 098; //int.parse(_callController.text.trim());
+                      String isServiceProvider = serviceProviderUnaccepted;
                       final UserModel user = UserModel(
                         call: call,
                         dateOfBirth: dateOfBirth,
                         email: email,
+                        role: 'user',
                         password: password,
                         fullName: fullName,
                         isServiceProvider: isServiceProvider,
                       );
-                      //"noor12@gmail.com";
-                      //07863425435
-                      await Future.delayed(const Duration(seconds: 3));
-                      final bool auth =
-                          await provider.signUp(user, _selectedImage);
-                      if (auth) {
+                      await Future.delayed(const Duration(seconds: 2));
+                      final auth = await provider.signUp(user, _selectedImage);
+                      if (auth is Success) {
                         navigatePushScreen(
                             context: context, screen: const ComplexScreen());
-                      } else {
-                        print(auth);
+                      }
+                      if (auth is Error) {
+                        setState(() {
+                          _loodaing = false;
+                        });
+
+                        styledScaffoldMessenger(
+                          context: context,
+                          message: 'تأكد من ملئ جميع الحقول',
+                          color: AppColor.buttonColorRed,
+                        );
                       }
                     },
                   ),
--- lib/components/form/sign_in_form.dart
+++ lib/components/form/sign_in_form.dart
@@ -1,10 +1,19 @@
+import 'dart:math';
+
 import 'package:announcement_of_services/components/custom_text_form_field.dart';
 import 'package:announcement_of_services/components/custom_buttom.dart';
 import 'package:announcement_of_services/module/auth_model.dart';
+import 'package:announcement_of_services/utils/constant/color.dart';
 import 'package:announcement_of_services/utils/navigate_utils.dart';
+import 'package:announcement_of_services/utils/styled_scaffold_messenger.dart';
+import 'package:announcement_of_services/utils/validate.dart';
 import 'package:announcement_of_services/view/complex_screen.dart';
+import 'package:announcement_of_services/view_model/app_status.dart';
 import 'package:announcement_of_services/view_model/view_model_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:progress_state_button/progress_button.dart';
+// import 'package:progress_state_button/iconed_button.dart';
+// import 'package:progress_state_button/progress_button.dart';
 import 'package:provider/provider.dart';
 
 class SignInForm extends StatefulWidget {
@@ -14,15 +23,26 @@ class SignInForm extends StatefulWidget {
   State<SignInForm> createState() => _SignInFormState();
 }
 
-final GlobalKey<FormState> _sigInFormKey = GlobalKey<FormState>();
-final TextEditingController _emailController = TextEditingController();
-final TextEditingController _passwordController = TextEditingController();
-
 class _SignInFormState extends State<SignInForm> {
+  final GlobalKey<FormState> _sigInFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  ButtonState stateOnlyText = ButtonState.idle;
+  ButtonState stateTextWithIcon = ButtonState.idle;
+  bool _isObscureText = true;
+  bool _loodaing = false;
+  AuthModel? _authData;
+  @override
+  void dispose() {
+    _sigInFormKey.currentState?.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<ViewModelAuth>(context, listen: false);
-    AuthModel _authData;
     return Form(
       key: _sigInFormKey,
       child: Column(
@@ -33,39 +53,94 @@ class _SignInFormState extends State<SignInForm> {
               children: [
                 CustomTextFormFiled(
                   context: context,
+                  readOnly: _loodaing ? true : false,
                   controller: _emailController,
                   headline: 'عنوان البريد الألكتروني',
                   hintText: 'إدخل البريد الألكتروني',
-                  textInputType: TextInputType.name,
-                  valid: (val) {},
+                  textInputType: TextInputType.emailAddress,
+                  valid: (String? value) {
+                    return ValidTextForm().email(value);
+                  },
                   icon: const Icon(Icons.person),
                 ),
                 CustomTextFormFiled(
                   context: context,
+                  isPassword: true,
+                  obscureText: _isObscureText,
+                  onPressedPassword: () {
+                    setState(() {
+                      _isObscureText = !_isObscureText;
+                    });
+                  },
                   controller: _passwordController,
+                  readOnly: _loodaing ? true : false,
                   headline: 'كلمة المرور',
                   hintText: 'إدخل كلمة المرور',
                   textInputType: TextInputType.visiblePassword,
-                  valid: (val) {},
+                  valid: (String? value) {
+                    return ValidTextForm().password(value);
+                  },
                   icon: const Icon(Icons.password_outlined),
                 ),
               ],
             ),
           ),
           CustomButton(
+            isLoad: _loodaing,
             context: context,
             title: 'تسجيل الدخول',
             onPressed: () async {
+              //  try {
+              if (!_sigInFormKey.currentState!.validate()) {
+                return;
+              }
+              setState(() {
+                _loodaing = true;
+              });
+              _sigInFormKey.currentState!.save();
               final String email = _emailController.text.trim();
               final String password = _passwordController.text.trim();
               _authData = AuthModel(
                 email: email,
                 password: password,
               ); //"noor12@gmail.com";
-              final auth = await provider.logIn(_authData);
-              if (auth) {
-                navigatePushScreen(context: context, screen: ComplexScreen());
+              //final bool auth = await provider.logIn(_authData!);
+               //await Future.delayed(const Duration(seconds: 3));
+              final auth = await provider.logIn(_authData!);
+              // print("aaaaa $auth");
+              if (auth is Success) {
+                print(
+                    "auth.code ${auth.code}: ${auth.response}"); //noor@gmail.com
+                // ignore: use_build_context_synchronously
+                navigatePushScreen(
+                    context: context, screen: const ComplexScreen());
+              } //} else {
+              // ignore: use_build_context_synchronously
+              if (auth is Error) {
+                setState(() {
+                  _loodaing = false;
+                });
+               // print("auth.code ${auth.code}: ${auth.errorResponse}");
+                // ignore: use_build_context_synchronously
+                styledScaffoldMessenger(
+                  context: context,
+                  message: 'تأكد من البريد الألكتروني أو كلمة المرور',
+                  color: AppColor.buttonColorRed,
+                );
               }
+              // }
+              // } else {
+              //   // ignore: use_build_context_synchronously
+              //  // print('No');
+              // styledScaffoldMessenger(
+              //   context: context,
+              //   message: 'تأكد من البريد الألكتروني و كلمة المرور',
+              //   color: AppColor.buttonColorRed,
+              // );
+              // }
+              // } catch (e) {
+              //   print("fsfsf ${e.toString()}");
+              // }
             },
           ),
         ],
@@ -73,3 +148,94 @@ class _SignInFormState extends State<SignInForm> {
     );
   }
 }
+
+
+
+
+
+
+ 
+
+
+// import this Package in pubspec.yaml file
+// dependencies:
+// 
+//   progress_state_button: ^1.0.2
+
+
+
+// class MyCustomWidget extends StatefulWidget {
+//   @override
+//   _MyCustomWidgetState createState() => _MyCustomWidgetState();
+// }
+
+// class _MyCustomWidgetState extends State<MyCustomWidget> {
+//   ButtonState stateOnlyText = ButtonState.idle;
+//   ButtonState stateTextWithIcon = ButtonState.idle;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: ProgressButton.icon(iconedButtons: {
+//           ButtonState.idle: IconedButton(
+//               text: 'Send',
+//               icon: const Icon(Icons.send, color: Colors.white),
+//               color: Colors.deepPurple.shade500,
+//               ),
+//           ButtonState.loading:
+//               IconedButton(text: 'Loading', color: Colors.deepPurple.shade700),
+//           ButtonState.fail: IconedButton(
+//               text: 'Failed',
+//               icon: const Icon(Icons.cancel, color: Colors.white),
+//               color: Colors.red.shade300,
+//               ),
+//           ButtonState.success: IconedButton(
+//               text: 'Success',
+//               icon: const Icon(
+//                 Icons.check_circle,
+//                 color: Colors.white,
+//               ),
+//               color: Colors.green.shade400)
+//         }, onPressed: onPressedIconWithText, state: stateTextWithIcon),
+//       ),
+//     );
+//   }
+
+//   void onPressedIconWithText() {
+//     switch (stateTextWithIcon) {
+//       case ButtonState.idle:
+//         stateTextWithIcon = ButtonState.loading;
+//         Future.delayed(
+//           const Duration(seconds: 1),
+//           () {
+//             setState(
+//               () {
+//                 stateTextWithIcon = Random.secure().nextBool()
+//                     ? ButtonState.success
+//                     : ButtonState.fail;
+//               },
+//             );
+//           },
+//         );
+
+//         break;
+//       case ButtonState.loading:
+//         break;
+//       case ButtonState.success:
+//         stateTextWithIcon = ButtonState.idle;
+//         break;
+//       case ButtonState.fail:
+//         stateTextWithIcon = ButtonState.idle;
+//         break;
+//     }
+//     setState(
+//       () {
+//         stateTextWithIcon = stateTextWithIcon;
+//       },
+//     );
+//   }
+// }
+
+
+
