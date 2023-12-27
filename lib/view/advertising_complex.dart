5451003fdc56b0543be5ecf06ab4a3db9bb3fdf5
Zonetto <as076053@gmail.com>
2023-12-27 10:11:17 +0300

    ADD advertising_complex.dart & loading_screen.dart

---
+++ lib/view/advertising_complex.dart
@@ -0,0 +1,48 @@
+import 'package:announcement_of_services/module/user_model.dart';
+import 'package:announcement_of_services/services/collections/user_collection.dart';
+import 'package:announcement_of_services/utils/constant/constants.dart';
+import 'package:announcement_of_services/view/admin_screen.dart';
+import 'package:announcement_of_services/view/advertisement_for_service_screen.dart';
+import 'package:announcement_of_services/view/wait_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'advertisement_for_service_view_screen.dart';
+
+class AdvertisingComplex extends StatelessWidget {
+   AdvertisingComplex({super.key});
+
+  final Stream<UserModel> stateIsServiceProvider =
+      UserCollection().stateIsServiceProvider()!;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<UserModel>(
+      stream: stateIsServiceProvider,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return const Text('');
+        } else {
+          final UserModel? userDetails = snapshot.data;
+
+          if (userDetails == null) {
+            return const Text('dd');
+          }
+
+          final String isServiceProvider = userDetails.isServiceProvider;
+          final String role = userDetails.role!;
+          print(role);
+
+          return role == "ADMIN"
+              ? const AdminScreen()
+              : isServiceProvider == serviceProviderUnaccepted
+                  ? const AdvertisementForServiceScreen()
+                  : isServiceProvider == serviceProviderAccept
+                      ? const AdvertisementForServiceInfoViewScreen()
+                      : const WaitScreen();
+        }
+      },
+    );
+  }
+}
