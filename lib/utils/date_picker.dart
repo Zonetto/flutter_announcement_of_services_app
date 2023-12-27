3e62436aba95680a37a79cd26719f81c9faccca5
Zonetto <as076053@gmail.com>
2023-12-27 10:21:07 +0300

    UPD date_picker.dart & validate.dart

--- lib/utils/date_picker.dart
+++ lib/utils/date_picker.dart
@@ -16,3 +16,35 @@ Future<String> datePicker({required BuildContext context}) async {
   }
   return '';
 }
+
+Future<String> timePicker({
+  required BuildContext context,
+}) async {
+  TimeOfDay currentTime = TimeOfDay.now();
+  final TimeOfDay? time = await showTimePicker(
+    initialEntryMode: TimePickerEntryMode.dialOnly,
+    context: context,
+    initialTime: currentTime,
+  );
+  if (time != null) {
+    String formattedTime = convert12Hours(time);
+    return formattedTime;
+  }
+  return '';
+}
+
+
+String convert12Hours(TimeOfDay time) {
+  int hour = time.hour;
+  int minute = time.minute;
+
+  String period = (hour >= 12) ? 'مساءً' : 'صباحً';
+  if (hour > 12) {
+    hour = hour - 12;
+  } else if (hour == 0) {
+    hour = 12;
+  }
+  String convertedTime = '$hour:${minute.toString().padLeft(2, '0')} $period';
+
+  return convertedTime;
+}
--- lib/utils/validate.dart
+++ lib/utils/validate.dart
@@ -1,17 +1,73 @@
 
-validTextForm(var value,int max,int min,String type){
-  
-  if( value.isEmpty ||value==null){
-    return "لا يمكن ترك الحقل فارغ";
+class ValidTextForm {
+  final String _empty = "لا يمكن ترك الحقل فارغ";
+  String? emptyText(
+    String? value,
+  ) {
+    if (value!.isEmpty) {
+      return _empty;
+    }
+    return null;
+  }
 
-}
+  String? email(
+    String? value,
+  ) {
+    if (value!.isEmpty) {
+      return emptyText(value);
+    }
+    if (!value.contains('@gmail')) {
+      return "يجب ان يحتوي email على @gmail";
+    }
+    if (!value.contains('.com')) {
+      return "يجب ان يحتوي email على com.";
+    }
+    return null;
+  }
 
- if(value.length > max){
-    return " رقم الهاتف  يجب ان يكون اصغر من$max رقم ";
+  String? password(
+    String? value,
+  ) {
+    if (value!.isEmpty) {
+      return emptyText(value);
+    }
+    if (value.length <= 5) {
+      return "يجب أن تكون كلمة المرور اكبر من 7 رموز";
+    }
+    return null;
+  }
 
-}
- if(value.length < min){
-    return "رقم الهاتف  يجب ان يتكون من$min رقم";
+  String? confirmPassword(
+    String? confirmPassword,
+    String? primaryPassword,
+  ) {
+    if (confirmPassword!.isEmpty) {
+      return emptyText(confirmPassword);
+    }
+    if (confirmPassword != primaryPassword) {
+      return "كلمة المرور غير متطابقة";
+    }
+    return null;
+  }
 
-}
+  String? nullText(
+    String? value,
+  ) {
+    if (value == null) {
+      return _empty;
+    }
+    return null;
+  }
+
+  String? call(
+    String? value,
+  ) {
+    if (value!.isEmpty) {
+      return emptyText(value);
+    }
+    if (value.length < 11) {
+      return "يجب ان يكون رقم الهاتف ١١ رقم";
+    }
+    return null;
+  }
 }
