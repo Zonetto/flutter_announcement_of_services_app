bba82e82520237e187b6b3e6ea74aa25e8359df5
Zonetto <as076053@gmail.com>
2024-01-18 21:27:28 +0300

    UPD and REN card_contents_widget.dart & text_widget.dart

--- lib/components/text_widget.dart
+++ lib/components/text_widget.dart
@@ -2,7 +2,7 @@ import 'package:announcement_of_services/utils/constant/color.dart';
 import 'package:announcement_of_services/utils/constant/responsive_screen.dart';
 import 'package:flutter/material.dart';
 
-class CustomText extends StatelessWidget {
+class TextWidget extends StatelessWidget {
   final String title;
   final double size;
   final TextAlign alignment;
@@ -11,7 +11,7 @@ class CustomText extends StatelessWidget {
   final int? maxLines;
   final TextOverflow? overflow;
 
-  const CustomText({
+  const TextWidget({
     super.key,
     required this.title,
     this.size = 18,
@@ -32,7 +32,7 @@ class CustomText extends StatelessWidget {
       //  style: Theme.of(context).textTheme.titleLarge,
       style: TextStyle(
         fontWeight: fontWeight,
-        color: color,
+      //  color: color,
         fontSize: Dimensions.fontSize(context, size),
       ),
     );
