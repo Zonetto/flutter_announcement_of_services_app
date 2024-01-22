94def3903c45a1435d8c63961161944d832d1197
Zonetto <as076053@gmail.com>
2024-01-22 10:29:28 +0300

    UPD main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,40 @@
-import 'dart:io';
-
-import 'package:announcement_of_services/view/complex_screen.dart';
+import 'package:announcement_of_services/utils/constant/color.dart';
+import 'package:announcement_of_services/view/splash_screen.dart';
 import 'package:announcement_of_services/view/start_screen.dart';
 import 'package:announcement_of_services/view_model/view_model_auth.dart';
-import 'package:announcement_of_services/view_model/view_model_fetch.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:announcement_of_services/view_model/view_model_fetch_user.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'generated/l10n.dart';
-import 'view/categories_creen.dart';
-import 'view/home_screen.dart';
-import 'view/loadingScreen.dart';
-import 'view/registration_option_screen.dart';
-import 'view/service_details_screen.dart';
+import 'view/bottom_navigation_bar_screen.dart';
+import 'view/lodaing_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid
-      ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-          apiKey: 'AIzaSyCnSlXad2dN_lYXEjg15uuY8C18L2hchqw',
-          appId: '1:173236485210:android:95e8fab76659b783faac63',
-          messagingSenderId: '173236485210',
-          projectId: 'flutterhomeservice-73056',
-        ))
-      : await Firebase.initializeApp();
-  await FirebaseAppCheck.instance.activate(
-    webProvider: ReCaptchaV3Provider('AIzaSyCnSlXad2dN_lYXEjg15uuY8C18L2hchqw'),
-    appleProvider: AppleProvider.appAttest,
-    androidProvider: AndroidProvider.debug,
+  // Platform.isAndroid
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyCnSlXad2dN_lYXEjg15uuY8C18L2hchqw',
+      appId: '1:173236485210:android:95e8fab76659b783faac63',
+      messagingSenderId: '173236485210',
+      projectId: 'flutterhomeservice-73056',
+    ),
   );
 
   runApp(
-    // Builder(
-    //   builder: (context) => MultiProvider(
-    //     providers: [
-    //       ChangeNotifierProvider<ViewModelAuth>(
-    //         create: (context) => ViewModelAuth(),
-    //         // child: MaterialApp(
-    //         //   locale: const Locale('ar'),
-    //         //   localizationsDelegates: const [
-    //         //     S.delegate,
-    //         //     GlobalMaterialLocalizations.delegate,
-    //         //     GlobalWidgetsLocalizations.delegate,
-    //         //     GlobalCupertinoLocalizations.delegate
-    //         //   ],
-    //         //   debugShowCheckedModeBanner: false,
-    //         //   supportedLocales: S.delegate.supportedLocales,
-    //         //   home: const MainApp(), // Use MainApp as the home widget
-    //         // ),
-    //       ),
-    //       ChangeNotifierProvider<ViewModelFetch>(
-    //         create: (context) => ViewModelFetch(),
-    //       )
-    //     ],
-    //   ),
-    // ),
+    const MyApp(),
+  );
+}
 
-    MultiProvider(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.dark);
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider<ViewModelAuth>(
           create: (_) => ViewModelAuth(),
@@ -68,39 +43,111 @@ Future<void> main() async {
           create: (_) => ViewModelFetch(),
         )
       ],
-      child: const MainApp(),
-    ),
-  );
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    //   return MaterialApp(home: const StartScreen());
-
-    return Consumer<ViewModelAuth>(
-      builder: (ctx, auth, _) => MaterialApp(
-        locale: const Locale('ar'),
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate
-        ],
-        debugShowCheckedModeBanner: false,
-        supportedLocales: S.delegate.supportedLocales,
-        home: auth.isAuth!
-            ? const ComplexScreen()
-            : FutureBuilder(
-                future: auth.tryAuthLogin(),
-                builder: (ctx, snapShot) =>
-                    (snapShot.connectionState == ConnectionState.waiting)
-                        ? const LoadingScreen()
-                        : const StartScreen(),
-              ), // Use MainApp as the home widget
+      child: Consumer<ViewModelAuth>(
+        builder: (ctx, auth, _) => ValueListenableBuilder(
+            valueListenable: themeNotifier,
+            builder: (_, currentMode, __) {
+              return MaterialApp(
+                theme: lightMode(),
+                darkTheme: darkMode(),
+                themeMode: currentMode,
+                locale: const Locale('ar'),
+                localizationsDelegates: const [
+                  S.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate
+                ],
+                debugShowCheckedModeBanner: false,
+                supportedLocales: S.delegate.supportedLocales,
+                home: auth.isAuth!
+                    ? const SplashScreen(
+                        route: BottomNavigationBarScreen(),
+                      )
+                    : FutureBuilder(
+                        future: auth.tryAuthLogin(),
+                        builder: (ctx, snapShot) => (snapShot.connectionState ==
+                                ConnectionState.waiting)
+                            ? const LoadingScreen()
+                            : const SplashScreen(
+                                route: StartScreen(),
+                              ),
+                      ),
+              );
+            }),
       ),
     );
   }
 }
+
+ThemeData lightMode() {
+  return ThemeData(
+    iconTheme: IconThemeData(color: AppColor.darkShadeText),
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: AppColor.green,
+    ),
+    secondaryHeaderColor: AppColor.grey,
+    splashColor: AppColor.white,
+    bottomAppBarColor: AppColor.green,
+    cardColor: AppColor.lightShade,
+    scaffoldBackgroundColor: AppColor.white,
+    hintColor: AppColor.colorTextBlack,
+    navigationBarTheme: NavigationBarThemeData(
+      backgroundColor: AppColor.white,
+      iconTheme: MaterialStateProperty.all<IconThemeData>(IconThemeData(
+        color: AppColor.darkShadeText,
+        size: 30,
+      )),
+      labelTextStyle: MaterialStateProperty.all<TextStyle>(
+        TextStyle(
+          color: AppColor.darkShadeText,
+          fontSize: 12,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+}
+
+ThemeData darkMode() {
+  return ThemeData.dark().copyWith(
+    //  textButtonTheme: TextButtonThemeData(style: ButtonStyle(backgroundColor: )),
+    iconTheme: const IconThemeData(color: AppColor.white),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: AppColor.white),
+    ),
+    bottomAppBarColor: AppColor.darkShade,
+    secondaryHeaderColor: AppColor.darkShadeText,
+    progressIndicatorTheme:
+        const ProgressIndicatorThemeData(color: AppColor.white),
+    splashColor: AppColor.darkShadeText,
+    hintColor: AppColor.whiteText,
+    cardColor: AppColor.darkShadeText,
+    scaffoldBackgroundColor: AppColor.darkShade,
+    navigationBarTheme: NavigationBarThemeData(
+      backgroundColor: AppColor.darkShade,
+      iconTheme: MaterialStateProperty.all<IconThemeData>(const IconThemeData(
+        color: AppColor.white,
+        size: 30,
+      )),
+      labelTextStyle: MaterialStateProperty.all<TextStyle>(
+        TextStyle(
+          color: AppColor.whiteText,
+          fontSize: 12,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+          // routes: {
+          //   "/LoadingScreen": (context) => const LoadingScreen(),
+          //   "/RegistrationOptionScreen": (context) =>
+          //       const RegistrationOptionScreen(),
+          //   "/StartScreen": (context) => const StartScreen(),
+          //   "/BottomNavigationBarScreen": (context) =>
+          //       const BottomNavigationBarScreen(),
+          // },
