d0a6908dc795fc41391095546fb81b7ef7124e84
Zonetto <as076053@gmail.com>
2024-01-23 09:16:14 +0300

    UPD custom_shape_decoration.dart

--- lib/components/shared/custom_shape_decoration.dart
+++ lib/components/shared/custom_shape_decoration.dart
@@ -2,12 +2,14 @@ import 'package:announcement_of_services/utils/constant/color.dart';
 import 'package:flutter/material.dart';
 
 class CustomShapeDecoration {
-  static ShapeDecoration shapeDecoration(BuildContext context) => ShapeDecoration(
-    color: Theme.of(context).splashColor,
-    // color: AppColor.screenBackGroundColor,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
-    ),
-    shadows: const [AppColor.shadow],
-  );
+  static ShapeDecoration shapeDecoration(BuildContext context,
+          {Color? color}) =>
+      ShapeDecoration(
+        color: color ?? Theme.of(context).splashColor,
+        // color: AppColor.screenBackGroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        shadows: [AppColor.shadow(context)],
+      );
 }
