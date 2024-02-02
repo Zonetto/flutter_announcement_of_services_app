3f512f142d2fd021447f98b836fbd7ef3bf24658
Zonetto <as076053@gmail.com>
2024-02-02 19:02:57 +0300

    UPD constants.dart

--- lib/utils/constant/constants.dart
+++ lib/utils/constant/constants.dart
@@ -1,6 +1,7 @@
 class CollectionDB {
   static const String userCollection = 'user_tbl';
   static const String servicesProviderCollection = 'service_provider_tbl';
+  static const String requestCollection = 'request_tbl';
   static const String postCollection = 'post_tbl';
 }
 
@@ -9,3 +10,7 @@ const String key = 'key=AIzaSyAte23mrZFcs16b_FufGlGesH_TCP7zPBc';
 const String serviceProviderAccept = "ACCEPT";
 const String serviceProviderUnaccepted = "UNACCEPTED";
 const String serviceProviderWait = "WAIT";
+const String REQUEST_WAIT = "WAIT";
+const String REQUEST_DONE = "DONE";
+const String REQUEST_ACCEPTED = "ACCEPTED";
+const String REQUEST_UNACCEPTED = "UNACCEPTED";
