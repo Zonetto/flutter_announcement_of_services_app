1fc7763fb83b7a4e11ea6ee5f28baaf62e991377
Zonetto <as076053@gmail.com>
2024-02-02 18:59:09 +0300

    ADD request_details_model.dart & request_model.dart

---
+++ lib/module/request_details_model.dart
@@ -0,0 +1,41 @@
+import 'package:announcement_of_services/module/user_details_model.dart';
+import 'package:announcement_of_services/utils/constant/constants.dart';
+
+class RequestDetailsModel {
+  String? requesId;
+  UserDetailsModel? serviceProviderDoc;
+  UserDetailsModel? userCollectionDoc;
+  final String desc;
+  final String date;
+  final String time;
+  final String requestDate;
+  String? status;
+
+  RequestDetailsModel({
+    this.requesId,
+    this.userCollectionDoc,
+    this.serviceProviderDoc,
+    required this.date,
+    required this.desc,
+    required this.time,
+    required this.requestDate,
+    this.status = REQUEST_WAIT,
+  });
+
+  factory RequestDetailsModel.fromJsonServiceProvider(
+    Map<String, dynamic> json, {
+    UserDetailsModel? servicesProvider,
+    UserDetailsModel? userCollectionDoc,
+  }) {
+    return RequestDetailsModel(
+      requesId: json["reques_id"],
+      serviceProviderDoc: servicesProvider,
+      userCollectionDoc: userCollectionDoc,
+      desc: json["desc"],
+      date: json["date"],
+      time: json["time"],
+      requestDate: json["request_date"],
+      status: json["status"],
+    );
+  }
+}
---
+++ lib/module/request_model.dart
@@ -0,0 +1,65 @@
+import 'package:announcement_of_services/utils/constant/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'service_provider_model.dart';
+import 'user_details_model.dart';
+
+class RequestModel {
+  String? requestId;
+  DocumentReference? serviceProviderCollection;
+  DocumentReference? userCollectionCollection;
+  final String desc;
+  final String date;
+  final String time;
+  final String requestDate;
+  String? status;
+
+  RequestModel({
+    this.requestId,
+    this.userCollectionCollection,
+    this.serviceProviderCollection,
+    required this.date,
+    required this.desc,
+    required this.time,
+    required this.requestDate,
+    this.status = REQUEST_WAIT,
+  });
+
+  factory RequestModel.fromJson(Map<String, dynamic> json) {
+    return RequestModel(
+      requestId: json["reques_id"],
+      serviceProviderCollection: json["service_provider_doc"],
+      userCollectionCollection: json["user_doc"],
+      desc: json["desc"],
+      date: json["date"],
+      time: json["time"],
+      requestDate: json["request_date"],
+      status: json["status"],
+    );
+  }
+  toJson() {
+    return {
+      "reques_id": requestId,
+      "service_provider_doc": serviceProviderCollection,
+      "user_doc": userCollectionCollection,
+      "desc": desc,
+      "date": date,
+      "time": time,
+      "request_date": requestDate,
+      "status": status,
+    };
+  }
+
+  Future<UserDetailsModel?> getServiceProviderModel(
+      ServicesProviderModel servicesProviderModel) async {
+    if (serviceProviderCollection != null) {
+      DocumentSnapshot<Object?> snapshot =
+          await serviceProviderCollection!.get();
+      if (snapshot.exists) {
+        return UserDetailsModel.fromDocumentSnapshot(
+            snapshot, servicesProviderModel);
+      }
+    }
+    return null; // Handle the case when serviceProviderCollection is null or document does not exist
+  }
+}
