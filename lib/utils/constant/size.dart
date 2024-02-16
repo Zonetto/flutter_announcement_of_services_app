8eed6d61f0ec175aeb696c6faa1ca59e7f3de97a
Zonetto <as076053@gmail.com>
2024-02-16 09:21:11 +0300

    UPD size.dart

--- lib/utils/constant/size.dart
+++ lib/utils/constant/size.dart
@@ -17,7 +17,7 @@ class AppSize {
 
   static double imageSizeSmall(BuildContext context) =>
       Dimensions.screenWidth(context) / 9.0;
-  static double imageSizeLarg(BuildContext context) =>
+  static double imageSizeLarge(BuildContext context) =>
       Dimensions.screenWidth(context) / 3.0;
 
 //  static const double internalMargin = 20.0;
